29cdfd954436fb447ba6ba29ee3a0811afdfc929
Ben Cates <ben.cates@gmail.com>
2021-12-17 00:06:35 -0700

    mix new

---
+++ lib/css_modules.ex
@@ -0,0 +1,18 @@
+defmodule CSSModules do
+  @moduledoc """
+  Documentation for `CSSModules`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CSSModules.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/css_modules_test.exs
@@ -0,0 +1,8 @@
+defmodule CSSModulesTest do
+  use ExUnit.Case
+  doctest CSSModules
+
+  test "greets the world" do
+    assert CSSModules.hello() == :world
+  end
+end
